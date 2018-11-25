ddbade9ea03e0c2b004f7c7cdb17ff8b23f8ed9d
ëwerton.costa <tominem@hotmail.com>
2018-11-25 21:39:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var argvs = process.argv.slice(2);
+if(argvs.length < 2) {
+    console.error('usage: strandum-clock-test <device-file-conf-path> <csv-dest-file>');
+    process.exit(1);
+}
+
+var device_conf_file = argvs[0];
+var csv_dest_file = argvs[1];
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+var t = [{name: 'teste', nota: 72.0},
+         {name: 'teste2', nota: 80.2}];
+
+var header = [];
+Object.keys(t[0]).forEach(k => header.push({id: k, title: k}));
+
+const csvWriter = createCsvWriter({
+    path: csv_dest_file,
+    header: header
+});
+ 
+csvWriter.writeRecords(t)  // returns a promise
+    .then(() => {
+        console.log('...Done');
+    });
